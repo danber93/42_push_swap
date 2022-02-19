e330ec6180d2ba34b03f31164fec51d090c3e7be
danber93 <daniele.bertillo@gmail.com>
2022-02-19 17:42:54 +0100

    fix push_swap.c inputs

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -1,5 +1,15 @@
 #include "push_swap.h"
 
+int		ft_n_args(char **av)
+{
+	int	i;
+
+	i = 0;
+	while (av[i])
+		i++;
+	return (i);
+}
+
 void	print_game(t_game *g)
 {
 	t_stack	*a;;
@@ -103,11 +113,75 @@ void	ft_print_error()
 	exit(0);
 }
 
+int		ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (0);
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	if (i == (int)ft_strlen(s1))
+		return (1);
+	return (0);
+}
+
+int		ft_repetions(char **av)
+{
+	int	i;
+	int	j;
+	int	n_args;
+
+	n_args = ft_n_args(av);
+	i = 1;
+	while (i < n_args)
+	{
+		j = i + 1;
+		while (j < n_args)
+		{
+			if (ft_strcmp(av[i], av[j]))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int		ft_bigger_maxint(char *str)
+{
+	int		i;
+	int		sign;
+	long	res;
+
+	res = 0;
+	i = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res *= 10;
+		res += str[i] - '0';
+		i++;
+		if (res > 2147483647)
+			return (1);
+	}
+	return (0);
+}
+
 int		ft_check_valid_input(char **av)
 {
 	int	i;
 	int	j;
 
+	if (ft_repetions(av))
+		return (0);
 	i = 1;
 	while (av[i])
 	{
@@ -116,7 +190,7 @@ int		ft_check_valid_input(char **av)
 		{
 			if (!(ft_isdigit(av[i][j]) || ft_isspace(av[i][j])))
 				return (0);
-			if (ft_strlen(av[i]) > 10 || ft_atoi(av[i]) > 2147483647)
+			if ((ft_bigger_maxint(av[i])))
 				ft_print_error();
 			j++;
 		}
@@ -135,7 +209,7 @@ char	**ft_add_empty(char **input)
 	len = 0;
 	while (input[len])
 		len++;
-	new = (char **)malloc(sizeof(char *) * (len + 1));
+	new = (char **)malloc(sizeof(char *) * (len + 2));
 	if (!new)
 		return (NULL);
 	new[0] = "./push_swap\0";
@@ -151,6 +225,7 @@ char	**ft_add_empty(char **input)
 		}
 		i++;
 	}
+	new[i + 1] = NULL;
 	free(input);
 	return new;
 }
@@ -160,28 +235,10 @@ char	**ft_check_string_input(char **av)
 	char	**input;
 	int		i;
 
-	if (!(ft_check_valid_input(av)))
-		return (NULL);
 	i = 0;
-	while(av[1][i])
-	{
-		if (!(ft_isspace(av[1][i])))
-			return (av);
-		i++;
-	}
 	input = ft_split(av[1], ' ');
 	input = ft_add_empty(input);
 	return (input);
-}
-
-int		ft_n_args(char **av)
-{
-	int	i;
-
-	i = 0;
-	while (av[i])
-		i++;
-	return (i);
 }
 
 int	main(int ac, char** av)
@@ -207,14 +264,12 @@ int	main(int ac, char** av)
 	ft_free_btree(root);
 	game = ft_init_game(av, ft_n_args(av), arr);
 	free(arr);
-	// print_game(game);
 	if (ft_n_args(av) == 4)
 		ft_stack_three(game);
 	else if (ft_n_args(av) == 6)
 		ft_stack_five(game);
 	else
 		ft_radix_sort(game);
-	// print_game(game);
 	ft_free_game(game);
 }
 
